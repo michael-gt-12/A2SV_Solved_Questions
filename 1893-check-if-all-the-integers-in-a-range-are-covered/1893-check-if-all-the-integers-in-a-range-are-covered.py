252f592483cadc541535dacdab3019c71dce6163
Michael <michaelgizachew38@gmail.com>
2026-02-05 09:12:12 +0300

    Time: 0 ms (100%), Space: 19.3 MB (40.45%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,5 +1,23 @@
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        # could not pass testcase 102 /106
+
+        # ranges.sort()
+        # pointer = 0
+        # while pointer < len(ranges):           
+        #     if left >= ranges[pointer][0]:
+        #         if right <= ranges[pointer][1]:
+        #             return True
+        #         else:
+        #             while pointer < len(ranges):
+        #                 if right <= ranges[pointer][1] and right >= ranges[pointer][0]:
+        #                     return True
+        #                 else:
+        #                     pointer += 1
+        #     else:
+        #         pointer += 1
+        # return False
+            
         for i in range(left,right+1):
             flag = False
             for j in range(len(ranges)):
