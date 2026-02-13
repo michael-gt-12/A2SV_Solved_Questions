f5f016401b1a9e64fcbeeeb8d1c6c557af2e5915
Michael <michaelgizachew38@gmail.com>
2026-02-13 16:09:50 +0300

    Add Beautiful Matrix program to calculate moves

---
+++ Beautiful Matrix.py
@@ -0,0 +1,11 @@
+matrix = []
+for i in range(5):
+    row = list(map(int,input().split()))
+    matrix.append(row)
+for i in range(5):
+    if 1 in matrix[i]:
+        row = i+1
+        column = matrix[i].index(1)+1
+        break
+moves = abs(row-3) + abs(column-3)
+print(moves)
