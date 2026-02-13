eaddd6d2052db12c10293249e75e2651ad09b048
Michael <michaelgizachew38@gmail.com>
2026-02-13 08:13:43 +0300

    Time: 224 ms (39.15%), Space: 37.3 MB (21.7%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,45 +1,24 @@
+from collections import Counter
+
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-        import copy
-        check = copy.deepcopy(changed)
-        result = []
-        for num in changed:
-            double = num * 2
-            if num in check and double in check:
-                if num == double:
-                    if check.count(num) == 1:
-                        continue
-                else:
-                    result.append(num)
-                    check.remove(num)
-                    check.remove(double)
-        if not check:
-            return result
-        else:
+        if len(changed) % 2 != 0:
             return []
-        # original_map = {}
-
-        # for num in changed:
-        #     original_map[num] = num * 2
         
-        # result = []
-
-        # for item in original_map.items():
-        #     if item[0] in changed and item[1] in changed:
-        #         print(item[0])
-        #         print(item[1])
-        #         result.append(item[0])
-        #         result.append(item[1])
-
-        # result.sort()
-        # changed.sort()
-
-        # if result == changed:
-        #     answer = []
-        #     for i in range(0,len(result),2):
-        #         answer.append(result[i])
-        #     return answer
-        # else:
-        #     return []
-
+        count = Counter(changed)
+        changed.sort()
         
+        result = []
+        
+        for x in changed:
+            if count[x] == 0:
+                continue
+            
+            if count[2 * x] <= 0:
+                return []
+            
+            result.append(x)
+            count[x] -= 1
+            count[2 * x] -= 1
+        
+        return result
