4feee03db111b55aff0468e937cbf2f517bd9a3b
Michael <michaelgizachew38@gmail.com>
2026-02-20 04:27:29 +0300

    Time: 0 ms (100%), Space: 19.2 MB (82.09%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -1,22 +1,21 @@
 class Solution:
     def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        import copy
         rows = len(mat)
-        cols = len(mat[0])
-        check = [[0]*cols for _ in range(rows)]
+
+        if mat == target:
+            return True
+
         count = 0
         while count < 3:
-            check = [[0]*cols for _ in range(rows)]
+            check = [[0]*rows for _ in range(rows)]
             for r in range(rows):
-                for c in range(cols):
+                for c in range(rows):
                     check[r][c] = mat[rows - 1 - c][r]
+
             if check == target:
                 return True
-            mat = copy.deepcopy(check)
+
+            mat = check
             count += 1
-        else:
-            return False
 
-
-
-        
+        return False
