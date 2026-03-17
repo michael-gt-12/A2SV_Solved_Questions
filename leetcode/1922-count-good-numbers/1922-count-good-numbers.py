d0cb94f3856967dd24503ffb67fcf4adb6e94c1b
Michael <michaelgizachew38@gmail.com>
2026-03-17 19:57:55 +0300

    Add solution for Count Good Numbers

---
+++ leetcode/1922-count-good-numbers/1922-count-good-numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        
+        MOD = 10 ** 9 + 7
+        even = (n+1) // 2
+        odd = (n) // 2
+
+        def pow(x, y):
+            if y == 0:
+                return 1
+            
+            if y % 2 == 0:
+                half = pow(x, y//2)
+                return (half * half) % MOD
+            else:
+                return (x * pow(x, y - 1)) % MOD
+
+        return (pow(5, even) * pow(4, odd)) % MOD
