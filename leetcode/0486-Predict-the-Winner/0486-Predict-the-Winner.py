bdaa67c2cda9f988d4127403a0eb26f009a76189
Michael <michaelgizachew38@gmail.com>
2026-03-19 17:53:33 +0300

    Add solution for Predict the Winner

---
+++ leetcode/0486-Predict-the-Winner/0486-Predict-the-Winner.py
@@ -0,0 +1,18 @@
+class Solution:
+    def predictTheWinner(self, nums):
+        memo = {}
+
+        def dfs(l, r):
+            if l == r:
+                return nums[l]
+
+            if (l, r) in memo:
+                return memo[(l, r)]
+
+            pick_left = nums[l] - dfs(l + 1, r)
+            pick_right = nums[r] - dfs(l, r - 1)
+
+            memo[(l, r)] = max(pick_left, pick_right)
+            return memo[(l, r)]
+
+        return dfs(0, len(nums) - 1) >= 0
