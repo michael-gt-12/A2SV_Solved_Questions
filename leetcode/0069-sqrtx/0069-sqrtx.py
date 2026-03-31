34d3bc27c75bc2f56c7fcbbfd12848b08feff1ac
Michael <michaelgizachew38@gmail.com>
2026-03-31 19:26:50 +0300

    Add solution for Sqrt(x)

---
+++ leetcode/0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 0
+        right = x
+        answer = -1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                right = mid -1
+            else:
+                answer = mid
+                left = mid + 1
+
+        return answer
+
+        
