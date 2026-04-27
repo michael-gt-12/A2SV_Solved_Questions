0f95c8a4bb4090b5392646cfeb7a09d2aacc180f
Michael <michaelgizachew38@gmail.com>
2026-04-27 18:04:46 +0300

    Add solution for Univalued Binary Tree

---
+++ leetcode/0965-univalued-binary-tree/0965-univalued-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        value = root.val
+        queue = deque()
+        queue.append(root)
+        while queue:
+            node = queue.pop()
+            if node.val != value:
+                return False
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            
+        return True
+        
